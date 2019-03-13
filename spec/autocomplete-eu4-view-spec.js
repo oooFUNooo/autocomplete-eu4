e9e5497d9caebad22c961745b20e14500ac18a46
FUN <fun.public.address@gmail.com>
2019-03-13 20:11:41 +0900

    initial commit

---
+++ spec/autocomplete-eu4-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AutocompleteEu4View from '../lib/autocomplete-eu4-view';
+
+describe('AutocompleteEu4View', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
